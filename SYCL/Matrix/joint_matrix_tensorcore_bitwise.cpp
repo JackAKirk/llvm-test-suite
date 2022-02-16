e5e0fdca6f3da7efd51644cf8ddacda0b98de10a
jack.kirk <jack.kirk@codeplay.com>
2022-02-16 10:59:23 +0000

    format

--- SYCL/Matrix/joint_matrix_tensorcore_bitwise.cpp
+++ SYCL/Matrix/joint_matrix_tensorcore_bitwise.cpp
@@ -118,49 +118,48 @@ void test(BinaryOperation Op) {
 
     cgh.parallel_for<KernelName<M, K, N, BinaryOperation>>(
         nd_range<2>(GlobalRange, LocalRange),
-        [=](nd_item<2> item)[[sycl::reqd_work_group_size(1, 1, 32)]] {
+        [=](nd_item<2> item) [[sycl::reqd_work_group_size(1, 1, 32)]] {
           sycl::sub_group sg = item.get_sub_group();
           const auto m =
               item.get_group()
                   .get_id()[0]; // row id of current submatrix of BIG C matrix
-          const auto n = item.get_group().get_id()[1]; // column id of current
+          const auto n =
+              item.get_group().get_id()[1]; // column id of current
                                                        // submatrix of BIG C matrix
           // matrix_use::a must have matrix_layout::row_major for single-bit
           // cases
-          joint_matrix<uint32_t, matrix_use::a, M, K,
-                       matrix_layout::row_major>
+          joint_matrix<uint32_t, matrix_use::a, M, K, matrix_layout::row_major>
               sub_a;
           // matrix_use::b must have matrix_layout::col_major for single-bit
           // cases
-          joint_matrix<uint32_t, matrix_use::b, K, N,
-                       matrix_layout::col_major>
+          joint_matrix<uint32_t, matrix_use::b, K, N, matrix_layout::col_major>
               sub_b;
 
           joint_matrix<int32_t, matrix_use::accumulator, M, N,
-                        matrix_layout::row_major>
-                sub_c;
+                      matrix_layout::row_major>
+              sub_c;
 
-            joint_matrix_load(
-                sg, sub_c, accC.get_pointer() + (m * M) * Big_N + n * N, Big_N);
+          joint_matrix_load(
+              sg, sub_c, accC.get_pointer() + (m * M) * Big_N + n * N, Big_N);
 
-            for (int k = 0; k < Sub_Tiles_K;
-                 k++) // row/col id of current submatrix of BIG A/B matrices
-            {
-              joint_matrix_load(sg, sub_a,
-                                accA_Packed.get_pointer() + (k * K / 32) +
-                                    (m * M * Big_K / 32),
-                                Big_K);
+          for (int k = 0; k < Sub_Tiles_K;
+                k++) // row/col id of current submatrix of BIG A/B matrices
+          {
+            joint_matrix_load(sg, sub_a,
+                              accA_Packed.get_pointer() + (k * K / 32) +
+                                  (m * M * Big_K / 32),
+                              Big_K);
 
-              joint_matrix_load(sg, sub_b,
-                                accB_Packed.get_pointer() + (n * N * Big_K / 32) +
-                                    (k * K / 32),
-                                Big_K);
+            joint_matrix_load(sg, sub_b,
+                              accB_Packed.get_pointer() + (n * N * Big_K / 32) +
+                                  (k * K / 32),
+                              Big_K);
 
-              sub_c = joint_matrix_bmad(sg, sub_a, sub_b, sub_c, Op);
-            }
-            joint_matrix_store(
-                sg, sub_c, accD.get_pointer() + (m * M) * Big_N + n * N, Big_N);
-          });
+            sub_c = joint_matrix_bmad(sg, sub_a, sub_b, sub_c, Op);
+          }
+          joint_matrix_store(
+              sg, sub_c, accD.get_pointer() + (m * M) * Big_N + n * N, Big_N);
+        });
   });
 
   q.wait();
