ee99178ec4c60e41fe76adcb4fef79d6fd912073
jack.kirk <jack.kirk@codeplay.com>
2022-02-18 11:34:34 +0000

    format

--- SYCL/Matrix/joint_matrix_tensorcore_bitwise.cpp
+++ SYCL/Matrix/joint_matrix_tensorcore_bitwise.cpp
@@ -80,11 +80,11 @@ void test(BinaryOperation Op) {
                        // "Big matrix" divided by 32.
 
 
-// Each bit of each uint32_t A/B array element is an element of a single-bit
-// matrix. joint_matrix_bmad performs Binary Dot Products on these matrices (see
-// M. Rastegari et al. Computer Vision – ECCV 2016, 525-542 and A. Li et al.
-// IEEE Transactions on Parallel and Distributed Systems, 32(7):1878-1891,
-// 2021))
+  // Each bit of each uint32_t A/B array element is an element of a single-bit
+  // matrix. joint_matrix_bmad performs Binary Dot Products on these matrices (see
+  // M. Rastegari et al. Computer Vision – ECCV 2016, 525-542 and A. Li et al.
+  // IEEE Transactions on Parallel and Distributed Systems, 32(7):1878-1891,
+  // 2021))
   uint32_t A[Big_M * Big_K];
   uint32_t B[Big_K * Big_N];
   int32_t C[Big_M * Big_N];
