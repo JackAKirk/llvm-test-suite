e84a7327e755126a91c4f227e5ac844684184b9f
JackAKirk <jack.kirk@codeplay.com>
2022-03-07 15:57:23 +0000

    removed additional line

--- SYCL/GroupAlgorithm/reduce_sycl2020.cpp
+++ SYCL/GroupAlgorithm/reduce_sycl2020.cpp
@@ -1,6 +1,5 @@
 // UNSUPPORTED: hip
 // RUN: %clangxx -fsycl -fsycl-targets=%sycl_triple -fsycl-device-code-split=per_kernel %s -I . -o %t.out
-
 // RUN: %CPU_RUN_PLACEHOLDER %t.out
 // RUN: %GPU_RUN_PLACEHOLDER %t.out
 // RUN: %ACC_RUN_PLACEHOLDER %t.out
