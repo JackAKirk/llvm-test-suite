512a37ab748843fb0673133ee150f08e3c6fb4c0
JackAKirk <chezjakirk@gmail.com>
2022-12-02 14:43:07 +0000

    Added fp64 aspect check.
    Signed-off-by: JackAKirk <jack.kirk@codeplay.com>

--- SYCL/DeviceLib/math_test_marray_vec.cpp
+++ SYCL/DeviceLib/math_test_marray_vec.cpp
@@ -13,14 +13,18 @@
 int main() {
   queue deviceQueue;
   math_tests_4<float4>(deviceQueue);
-  math_tests_4<double4>(deviceQueue);
   math_tests_4<marray<float, 4>>(deviceQueue);
-  math_tests_4<marray<double, 4>>(deviceQueue);
 
   math_tests_3<float3>(deviceQueue);
-  math_tests_3<double3>(deviceQueue);
   math_tests_3<marray<float, 3>>(deviceQueue);
-  math_tests_3<marray<double, 3>>(deviceQueue);
+
+  if (deviceQueue.get_device().has(sycl::aspect::fp64)) {
+    math_tests_4<double4>(deviceQueue);
+    math_tests_4<marray<double, 4>>(deviceQueue);
+
+    math_tests_3<double3>(deviceQueue);
+    math_tests_3<marray<double, 3>>(deviceQueue);
+  }
 
   std::cout << "Pass" << std::endl;
   return 0;
